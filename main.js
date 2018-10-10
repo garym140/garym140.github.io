afec9a9bb986b543449c5bc1eabe42b753100af5
garym140 <30574513+garym140@users.noreply.github.com>
2018-10-09 22:38:43 -0400

    Adding Animations 1

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/typeit@VERSION_NUMBER/dist/typeit.min.js" />
 <script src="../p5.min.js"></script>
+<script src="anime.min.js"></script>
 
 let t = 0; // time variable
 
@@ -30,4 +31,21 @@ function draw() {
   }
 
   t = t + 0.01; // update time
+}
+
+var pathEls = document.querySelectorAll('path');
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  var offset = anime.setDashoffset(pathEl);
+  pathEl.setAttribute('stroke-dashoffset', offset);
+  anime({
+    targets: pathEl,
+    strokeDashoffset: [offset, 0],
+    duration: anime.random(1000, 3000),
+    delay: anime.random(0, 2000),
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutSine',
+    autoplay: true
+  });
 }
